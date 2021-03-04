b6609888140146e11bae49c89e13a7d471c30994
Pushpa Kumari <68471073+pushpakumari5117@users.noreply.github.com>
2021-03-04 18:07:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2500)
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.pexels.com/photos/36753/flower-purple-lical-blosso.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />'
+  title.innerHTML = 'Lorem ipsum dolor sit amet'
+  excerpt.innerHTML =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
+  profile_img.innerHTML =
+    '<img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />'
+  name.innerHTML = 'John Doe'
+  date.innerHTML = 'Oct 08, 2020'
+
+  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+}
